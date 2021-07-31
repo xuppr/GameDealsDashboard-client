00ef64528b05d8e26b7201f78b86953bf4c7377c
Lorenzo Calvello <lo.cal_y@yahoo.com>
2021-07-31 13:09:01 +0200

    format DealCard and DealCard tests

--- src/components/deal-card/DealCard.js
+++ src/components/deal-card/DealCard.js
@@ -1,24 +1,26 @@
-import './DealCard.css'
+import "./DealCard.css";
 
 const storeClassMap = {
-  '1': 'steam-class',
-  '7': 'gog-class',
-  '11': 'humble-class'
-}
+  1: "steam-class",
+  7: "gog-class",
+  11: "humble-class",
+};
 
-const DealCard = ({dealData}) => {
-  const {title, storeID, salePrice, normalPrice, thumb, dealID} = dealData
-  const storeClass = storeClassMap[storeID]
-  const linkClass = dealID ? 'link-class' : ''
+const DealCard = ({ dealData }) => {
+  const { title, storeID, salePrice, normalPrice, thumb, dealID } = dealData;
+  const storeClass = storeClassMap[storeID];
+  const linkClass = dealID ? "link-class" : "";
 
-  return <div className={`${storeClass} ${linkClass} deal-card`}>
-            <div className='thumb-container'>
-              <img src={thumb} alt={title + ' image'}></img>
-            </div>
-            <h3>{title}</h3>
-            <h1>{salePrice}$</h1>
-            <p>Instead of {normalPrice}$</p>
-        </div>
-}
+  return (
+    <div className={`${storeClass} ${linkClass} deal-card`}>
+      <div className="thumb-container">
+        <img src={thumb} alt={title + " image"}></img>
+      </div>
+      <h3>{title}</h3>
+      <h1>{salePrice}$</h1>
+      <p>Instead of {normalPrice}$</p>
+    </div>
+  );
+};
 
-export default DealCard
+export default DealCard;
--- src/components/deal-card/DealCard.test.js
+++ src/components/deal-card/DealCard.test.js
@@ -1,116 +1,116 @@
-import { render, screen } from '@testing-library/react';
-import DealCard from './DealCard'
+import { render, screen } from "@testing-library/react";
+import DealCard from "./DealCard";
 
-describe('DealCard data', () => {
-
-  it('shows the data passed as prop', () => {
-  
+describe("DealCard data", () => {
+  it("shows the data passed as prop", () => {
     const dealData = {
       title: "Deus Ex: Human Revolution - Director's Cut",
       storeID: "11",
       salePrice: 2.99,
       normalPrice: 19.99,
-      thumb: "https:\/\/cdn.cloudflare.steamstatic.com\/steam\/apps\/238010\/capsule_sm_120.jpg?t=1619788192"
-    }
-  
-    render(<DealCard dealData={dealData}/>)
-  
-    const titleElement = screen.getByText(/Deus Ex: Human Revolution - Director's Cut/i)
-    expect(titleElement).toBeInTheDocument()
-  
-    const salePriceElement = screen.getByText("2.99$")
-    expect(salePriceElement).toBeInTheDocument()
-  
-    const normalPriceElement = screen.getByText("Instead of 19.99$")
-    expect(normalPriceElement).toBeInTheDocument()
-  
-    const imgElement = screen.getByRole('img')
-    expect(imgElement).toBeInTheDocument()
+      thumb:
+        "https://cdn.cloudflare.steamstatic.com/steam/apps/238010/capsule_sm_120.jpg?t=1619788192",
+    };
+
+    render(<DealCard dealData={dealData} />);
+
+    const titleElement = screen.getByText(
+      /Deus Ex: Human Revolution - Director's Cut/i
+    );
+    expect(titleElement).toBeInTheDocument();
+
+    const salePriceElement = screen.getByText("2.99$");
+    expect(salePriceElement).toBeInTheDocument();
+
+    const normalPriceElement = screen.getByText("Instead of 19.99$");
+    expect(normalPriceElement).toBeInTheDocument();
+
+    const imgElement = screen.getByRole("img");
+    expect(imgElement).toBeInTheDocument();
     expect(imgElement.src).toEqual(
-      "https:\/\/cdn.cloudflare.steamstatic.com\/steam\/apps\/238010\/capsule_sm_120.jpg?t=1619788192")
-    
-  })
-})
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/238010/capsule_sm_120.jpg?t=1619788192"
+    );
+  });
+});
 
-
-describe('DealCard style',() => {
-
-  it('sets the correct color for steam', () => {
+describe("DealCard style", () => {
+  it("sets the correct color for steam", () => {
     const steamData = {
       title: "Tumblestone",
       storeID: "1",
       salePrice: 3.74,
-      normalPrice : 24.99,
-      thumb : "https:\/\/cdn.cloudflare.steamstatic.com\/steam\/apps\/269710\/capsule_sm_120.jpg?t=1625169247"
-    }
+      normalPrice: 24.99,
+      thumb:
+        "https://cdn.cloudflare.steamstatic.com/steam/apps/269710/capsule_sm_120.jpg?t=1625169247",
+    };
 
-    const { container } = render(<DealCard dealData={steamData} />)
+    const { container } = render(<DealCard dealData={steamData} />);
 
-    expect(container.firstChild).toHaveClass('steam-class')
-    expect(container.firstChild.classList.contains('gog-class')).toBe(false)
-    expect(container.firstChild.classList.contains('humble-class')).toBe(false)
+    expect(container.firstChild).toHaveClass("steam-class");
+    expect(container.firstChild.classList.contains("gog-class")).toBe(false);
+    expect(container.firstChild.classList.contains("humble-class")).toBe(false);
+  });
 
-    
-  })
-
-  it('sets the correct color for gog', () => {
+  it("sets the correct color for gog", () => {
     const gogData = {
-      "title": "Freedom Force",
-      "storeID": "7",
-      "salePrice": 1.49,
-      "normalPrice": 5.99,
-      "thumb": "https:\/\/cdn.cloudflare.steamstatic.com\/steam\/apps\/8880\/capsule_sm_120.jpg?t=1569012854"
-    }
+      title: "Freedom Force",
+      storeID: "7",
+      salePrice: 1.49,
+      normalPrice: 5.99,
+      thumb:
+        "https://cdn.cloudflare.steamstatic.com/steam/apps/8880/capsule_sm_120.jpg?t=1569012854",
+    };
 
-    const { container } = render(<DealCard dealData={gogData} />)
+    const { container } = render(<DealCard dealData={gogData} />);
 
-    expect(container.firstChild).toHaveClass('gog-class')
-    expect(container.firstChild.classList.contains('steam-class')).toBe(false)
-    expect(container.firstChild.classList.contains('humble-class')).toBe(false)
-  })
+    expect(container.firstChild).toHaveClass("gog-class");
+    expect(container.firstChild.classList.contains("steam-class")).toBe(false);
+    expect(container.firstChild.classList.contains("humble-class")).toBe(false);
+  });
 
-  it('sets the correct color for humble store', () => {
+  it("sets the correct color for humble store", () => {
     const humbleData = {
       title: "Deus Ex: Human Revolution - Director's Cut",
       storeID: "11",
       salePrice: 2.99,
       normalPrice: 19.99,
-      thumb: "https:\/\/cdn.cloudflare.steamstatic.com\/steam\/apps\/238010\/capsule_sm_120.jpg?t=1619788192"
-    }
+      thumb:
+        "https://cdn.cloudflare.steamstatic.com/steam/apps/238010/capsule_sm_120.jpg?t=1619788192",
+    };
 
-    const { container } = render(<DealCard dealData={humbleData} />)
+    const { container } = render(<DealCard dealData={humbleData} />);
 
-    expect(container.firstChild).toHaveClass('humble-class')
-    expect(container.firstChild.classList.contains('steam-class')).toBe(false)
-    expect(container.firstChild.classList.contains('gog-class')).toBe(false)
-  })
+    expect(container.firstChild).toHaveClass("humble-class");
+    expect(container.firstChild.classList.contains("steam-class")).toBe(false);
+    expect(container.firstChild.classList.contains("gog-class")).toBe(false);
+  });
 
-  it('is hoverable when dealData contains a dealID', () => {
+  it("is hoverable when dealData contains a dealID", () => {
     const dealData = {
       title: "Deus Ex: Human Revolution - Director's Cut",
       dealID: "mU%2FbH6z0MsHtcyqBBnv1C29aei%2FU0ZcsW0tNaZjC3xQ%3D",
       storeID: "11",
       salePrice: 2.99,
       normalPrice: 19.99,
-      thumb: "https:\/\/cdn.cloudflare.steamstatic.com\/steam\/apps\/238010\/capsule_sm_120.jpg?t=1619788192"
-    }
+      thumb:
+        "https://cdn.cloudflare.steamstatic.com/steam/apps/238010/capsule_sm_120.jpg?t=1619788192",
+    };
 
-    const { container } = render(<DealCard dealData={dealData}/>)
-    expect(container.firstChild).toHaveClass('link-class')
-  })
+    const { container } = render(<DealCard dealData={dealData} />);
+    expect(container.firstChild).toHaveClass("link-class");
+  });
 
-  it('is not hoverable when dealData has  dealID', () => {
+  it("is not hoverable when dealData has  dealID", () => {
     const dealData = {
       title: "Deus Ex: Human Revolution - Director's Cut",
       storeID: "11",
       salePrice: 2.99,
       normalPrice: 19.99,
-      thumb: "https:\/\/cdn.cloudflare.steamstatic.com\/steam\/apps\/238010\/capsule_sm_120.jpg?t=1619788192"
-    }
-     
-    const { container } = render(<DealCard dealData={dealData} />)
-    expect(container.firstChild.classList.contains('link-class')).toBe(false)
-  })
+      thumb:
+        "https://cdn.cloudflare.steamstatic.com/steam/apps/238010/capsule_sm_120.jpg?t=1619788192",
+    };
 
-
-})
+    const { container } = render(<DealCard dealData={dealData} />);
+    expect(container.firstChild.classList.contains("link-class")).toBe(false);
+  });
+});
