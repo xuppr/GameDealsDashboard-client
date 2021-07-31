573e317642fa1f2f54a39cbe5d3b204114df0185
Lorenzo Calvello <lo.cal_y@yahoo.com>
2021-07-31 11:54:58 +0200

    add DealCard tests + pass DealCard tests + style

---
+++ src/components/deal-card/DealCard.js
@@ -0,0 +1,24 @@
+import './DealCard.css'
+
+const storeClassMap = {
+  '1': 'steam-class',
+  '7': 'gog-class',
+  '11': 'humble-class'
+}
+
+const DealCard = ({dealData}) => {
+  const {title, storeID, salePrice, normalPrice, thumb, dealID} = dealData
+  const storeClass = storeClassMap[storeID]
+  const linkClass = dealID ? 'link-class' : ''
+
+  return <div className={`${storeClass} ${linkClass} deal-card`}>
+            <div className='thumb-container'>
+              <img src={thumb}></img>
+            </div>
+            <h3>{title}</h3>
+            <h1>{salePrice}$</h1>
+            <p>Instead of {normalPrice}$</p>
+        </div>
+}
+
+export default DealCard
