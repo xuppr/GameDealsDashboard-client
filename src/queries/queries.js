fb8d11c07b3d4e7e0fd9737fd4e41a8c618388a2
Lorenzo Calvello <lo.cal_y@yahoo.com>
2021-08-01 00:19:05 +0200

    add queries

--- src/queries/queries.js
+++ src/queries/queries.js
@@ -30,4 +30,86 @@ const DEALS_QUERY = gql`
   }
 `;
 
+const DEALS_FILTERED_BY_STORE_QUERY = gql`
+  query dealsFilteredByStore($start: Int, $storeID: String) {
+    dealsFilteredByStore(start: $start, storeID: $storeID) {
+      dealsList {
+        title
+        storeID
+        dealID
+        salePrice
+        normalPrice
+        thumb
+      }
+      isEnd
+    }
+  }
+`;
+
+const DEALS_FILTERED_BY_PRICE_RANGE_QUERY = gql`
+  query dealsFilteredByPriceRange($start: Int, $lowPrice: Float, $highPrice: Float) {
+    dealsFilteredByPriceRange(start: $start, lowPrice: $lowPrice, highPrice: $highPrice) {
+      dealsList {
+        title
+        storeID
+        dealID
+        salePrice
+        normalPrice
+        thumb
+      }
+      isEnd
+    }
+  }
+`;
+
+const DEALS_SORTED_BY_PRICE_QUERY = gql`
+  query dealsSortedByPrice($start: Int) {
+    dealsSortedByPrice(start: $start) {
+      dealsList {
+        title
+        storeID
+        dealID
+        salePrice
+        normalPrice
+        thumb
+      }
+      isEnd
+    }
+  }
+`;
+
+const DEALS_SORTED_BY_SAVINGS_QUERY = gql`
+  query dealsSortedBySavings($start: Int) {
+    dealsSortedBySavings(start: $start) {
+      dealsList {
+        title
+        storeID
+        dealID
+        salePrice
+        normalPrice
+        thumb
+      }
+      isEnd
+    }
+  }
+`;
+
+const DEALS_SORTED_BY_DEAL_RATING_QUERY = gql`
+  query dealsSortedByDealRating($start: Int) {
+    dealsSortedByDealRating(start: $start) {
+      dealsList {
+        title
+        storeID
+        dealID
+        salePrice
+        normalPrice
+        thumb
+      }
+      isEnd
+    }
+  }
+`;
+
+
+
 export { ONE_PER_STORE_QUERY, DEALS_QUERY };
