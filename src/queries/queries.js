8f58c3afa2ceaef98defb9e86c273c22dbf7ef75
Lorenzo Calvello <lo.cal_y@yahoo.com>
2021-08-01 17:53:15 +0200

    update queries

--- src/queries/queries.js
+++ src/queries/queries.js
@@ -15,8 +15,8 @@ const ONE_PER_STORE_QUERY = gql`
 
 // DefaultContent
 const DEALS_QUERY = gql`
-  query deals($start: Int) {
-    deals(start: $start) {
+  query deals($start: Int, $storeID: String, $lowPrice: Float, $highPrice: Float, $sortBy: String) {
+    deals(start: $start, storeID: $storeID, lowPrice: $lowPrice, highPrice: $highPrice, sortBy: $sortBy) {
       dealsList {
         title
         storeID
@@ -29,95 +29,9 @@ const DEALS_QUERY = gql`
     }
   }
 `;
-
-const DEALS_FILTERED_BY_STORE_QUERY = gql`
-  query dealsFilteredByStore($start: Int, $storeID: String) {
-    dealsFilteredByStore(start: $start, storeID: $storeID) {
-      dealsList {
-        title
-        storeID
-        dealID
-        salePrice
-        normalPrice
-        thumb
-      }
-      isEnd
-    }
-  }
-`;
-
-const DEALS_FILTERED_BY_PRICE_RANGE_QUERY = gql`
-  query dealsFilteredByPriceRange($start: Int, $lowPrice: Float, $highPrice: Float) {
-    dealsFilteredByPriceRange(start: $start, lowPrice: $lowPrice, highPrice: $highPrice) {
-      dealsList {
-        title
-        storeID
-        dealID
-        salePrice
-        normalPrice
-        thumb
-      }
-      isEnd
-    }
-  }
-`;
-
-const DEALS_SORTED_BY_PRICE_QUERY = gql`
-  query dealsSortedByPrice($start: Int) {
-    dealsSortedByPrice(start: $start) {
-      dealsList {
-        title
-        storeID
-        dealID
-        salePrice
-        normalPrice
-        thumb
-      }
-      isEnd
-    }
-  }
-`;
-
-const DEALS_SORTED_BY_SAVINGS_QUERY = gql`
-  query dealsSortedBySavings($start: Int) {
-    dealsSortedBySavings(start: $start) {
-      dealsList {
-        title
-        storeID
-        dealID
-        salePrice
-        normalPrice
-        thumb
-      }
-      isEnd
-    }
-  }
-`;
-
-const DEALS_SORTED_BY_DEAL_RATING_QUERY = gql`
-  query dealsSortedByDealRating($start: Int) {
-    dealsSortedByDealRating(start: $start) {
-      dealsList {
-        title
-        storeID
-        dealID
-        salePrice
-        normalPrice
-        thumb
-      }
-      isEnd
-    }
-  }
-`;
-
 
 
 export { 
   ONE_PER_STORE_QUERY, 
   DEALS_QUERY,
-  DEALS_FILTERED_BY_STORE_QUERY,
-  DEALS_FILTERED_BY_PRICE_RANGE_QUERY,
-  DEALS_SORTED_BY_PRICE_QUERY,
-  DEALS_SORTED_BY_SAVINGS_QUERY,
-  DEALS_SORTED_BY_DEAL_RATING_QUERY 
 };
