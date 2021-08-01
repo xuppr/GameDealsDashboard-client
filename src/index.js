9a188cd40df1c9feec748df57c9bf33953f4f10e
Lorenzo Calvello <lo.cal_y@yahoo.com>
2021-08-01 19:08:51 +0200

    setting up apollo provider

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  ApolloLink,
+  from,
+  HttpLink,
+} from "@apollo/client";
+
+const authLink = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem("token");
+
+  console.log("auth link called!");
+  console.log(operation);
+
+  operation.setContext(({ headers }) => ({
+    headers: {
+      authorization: token ? `JWT ${token}` : "",
+      ...headers,
+    },
+  }));
+
+  return forward(operation);
+});
+
+const httpLink = new HttpLink({
+  uri: "http://localhost:8000/graphql/",
+  credentials: "include",
+});
+
+const additiveLink = from([authLink, httpLink]);
+
+const apolloClient = new ApolloClient({
+  link: additiveLink,
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={apolloClient}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
